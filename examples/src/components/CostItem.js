d4c679421afef6790fa985240fd954d5c253dc6c
Viktor <f49411151@gmail.com>
2023-07-04 14:18:14 +0300

    передача данных  транзитом в другой компонент

--- examples/src/components/CostItem.js
+++ examples/src/components/CostItem.js
@@ -4,6 +4,7 @@ import CostDate from "./CostDate";
 
 function CostItem(props) {
     return (<div className='cost-item'>
+                {/* Передача данных транзитом в другой компонент */}
                 <CostDate date={props.date}/>
                 <div className='cost-item__description'>
                     <h2>{props.description}</h2>
--- examples/src/components/Costs.js
+++ examples/src/components/Costs.js
@@ -1,4 +1,3 @@
-// Импортируем компонент CostItem. Он нужен только для того чтобы вернуть заданную разметку в App.js
 import CostItem from "./CostItem";
 import './Costs.css';
 
@@ -6,7 +5,6 @@ function Costs(props) {
 
      return (
          <div className='costs'>
-              {/* Синтаксис обращения к переданым ранее "реквизитам" props */}
               <CostItem date={props.costs[0].date} description={props.costs[0].description} amount={props.costs[0].amount}/>
               <CostItem date={props.costs[1].date} description={props.costs[1].description} amount={props.costs[1].amount}/>
               <CostItem date={props.costs[2].date} description={props.costs[2].description} amount={props.costs[2].amount}/>
