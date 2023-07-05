85234cca147a7e9340c22dd64525f17a61636ccb
Viktor <f49411151@gmail.com>
2023-07-05 21:35:46 +0300

    изменения по событию компонента хуком useState

--- examples/src/components/Costs/CostItem.js
+++ examples/src/components/Costs/CostItem.js
@@ -2,22 +2,28 @@ import './CostItem.css';
 import './CostDate';
 import CostDate from "./CostDate";
 import Card from "../UI/Card";
+// Для того чтобы React после отрисовки страницы отражал изменения (парсил элемент заново)
+// необходимо сделать именованный импорт функции useState из библиотеки React в элемент где стоят обработчики событий
+import React, { useState } from "react";
 
 const CostItem = (props) => {
-    // Функция для которая запускается при нажатии на элемент объявляется для чистоты кода заранее
+    // Чтобы useState(); сработал нужно его вызвать внутри компонента попутно вытащить из него значения деструктуризацией
+    const [description, setDescription] = useState(props.description);
+
+
     const changeDescriptionHandler = () => {
-        console.log('Click Clack')
+        // Здесь в функцию передаем значение которое нужно установить в переменной
+        // идет присваивание переменной нового значения + вызов компонента
+        // description='New cost'; & CostItem(props);
+        setDescription('New cost');
     }
 
     return (<Card className='cost-item'>
                 <CostDate date={props.date}/>
                 <div className='cost-item__description'>
-                    <h2>{props.description}</h2>
+                    <h2>{description}</h2>
                     <div className='cost-item__price'>${props.amount}</div>
                 </div>
-            {/* В синтаксисе React таким образом добавляется слушатель событий на элемент. Вызов функции ()
-            не ставим иначе она запустится при парсинге страницы. Название функции для обработчика событий желательно
-            заканчивать на Handler. Таким образом логически понятно что это функция предназначена для обработчика */}
             <button onClick={changeDescriptionHandler}>Изменить описание</button>
             </Card>
     );
